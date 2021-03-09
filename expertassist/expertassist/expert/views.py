c00766d6f043450a82535d3b0715cd2684b36222
jakupierblina <33768451+jakupierblina@users.noreply.github.com>
2021-03-09 15:35:26 +0100

    Update views.py

--- expertassist/expertassist/expert/views.py
+++ expertassist/expertassist/expert/views.py
@@ -5,77 +5,95 @@ from django.core.files.storage import FileSystemStorage
 import os
 
 from django.contrib import messages
+
 import pandas as pd
+
 # Create your views here.
 
+
+#dict ={}
 def index(request):
 
-    global attributeid
-    context={}
+    context = {}
+    global attribute
 
     if request.method == 'POST':
-        uploaded_file = request.FILES['document']
-        attributeid = request.POST.get('attributeid')
 
-        #print(uploaded_file)
-        #print(attributeid)
+        uploaded_file = request.FILES['document']
+        attribute = request.POST.get('attributeid')
+
+        print(attribute)
+
+        #check if this file ends with csv
         if uploaded_file.name.endswith('.csv'):
-            #save the file in media folder
             savefile = FileSystemStorage()
 
-            name = savefile.save(uploaded_file.name, uploaded_file) # this is the name of the file
+            name = savefile.save(uploaded_file.name, uploaded_file) #gets the name of the file
+            print(name)
 
-            #know where to save file
-            d = os.getcwd() #current directory of the project
-            file_directory = d+'\media\\'+name
 
+            #we need to save the file somewhere in the project, MEDIA
+            #now lets do the savings
+
+            d = os.getcwd() # how we get the current dorectory
+            file_directory = d+'\media\\'+name #saving the file in the media directory
+            print(file_directory)
             readfile(file_directory)
-            return redirect(results)
+
+            request.session['attribute'] = attribute
+
+            if attribute not in data.axes[1]:
+                messages.warning(request, 'Please write the column name correctly')
+            else:
+                print(attribute)
+                return redirect(results)
+
         else:
-            messages.warning(request, ' File was not uploaded. Please use csv file extension!')
+            messages.warning(request, 'File was not uploaded. Please use .csv file extension!')
 
 
+    return  render(request, 'index.html', context)
 
 
-    return  render(request, 'index.html')
-
-            #project.csv
+            #project_data.csv
 def readfile(filename):
 
+    #we have to create those in order to be able to access it around
+    # use panda to read the file because i can use DATAFRAME to read the file
+    #column;culumn2;column
     global rows,columns,data,my_file,missing_values
 
-    my_file = pd.read_csv(filename, sep='[:;,|_]', engine ='python')
+    my_file = pd.read_csv(filename, sep='[:;,|_]', engine='python')
 
     data = pd.DataFrame(data=my_file, index=None)
-
     print(data)
 
-    #rows and columns
     rows = len(data.axes[0])
     columns = len(data.axes[1])
 
-    #find missing data
-    missingsings = ['?', '0', '--']
-    null_data = data[data.isnull().any(axis=1)] # find the missing data
+    #read the missing data - checking if there is a null
+    missingvalue = ['?', '0', '--']
 
+    null_data = data[data.isnull().any(axis=1)] # find where is the missing data #na null =['x1','x13']
     missing_values = len(null_data)
 
+
+
 def results(request):
-
-    message = 'I found ' + str(rows) + ' rows and ' + str(columns) + ' columns. Missing data are: ' + str(missing_values)
-
+    # prepare the visualization
+                                #12
+    message = 'I found ' + str(rows) + ' rows and ' + str(columns) + ' columns. Missing data: ' + str(missing_values)
     messages.warning(request, message)
 
-    #split into keys and values based on the attribute input
-    dashboard = []
-
-    for x in data[attributeid]:
+    dashboard = [] # ['A11','A11',A'122',]
+    for x in data[attribute]:
         dashboard.append(x)
 
-    my_dashboard = dict(Counter(dashboard))
-    print('my dashboard ',my_dashboard)
+    my_dashboard = dict(Counter(dashboard)) #{'A121': 282, 'A122': 232, 'A124': 154, 'A123': 332}
 
-    keys = my_dashboard.keys()
+    print(my_dashboard)
+
+    keys = my_dashboard.keys() # {'A121', 'A122', 'A124', 'A123'}
     values = my_dashboard.values()
 
     listkeys = []
@@ -87,10 +105,12 @@ def results(request):
     for y in values:
         listvalues.append(y)
 
+    print(listkeys)
+    print(listvalues)
 
     context = {
-        'listkeys':listkeys,
-        'listvalues':listvalues,
-
+        'listkeys': listkeys,
+        'listvalues': listvalues,
     }
-    return  render(request, 'results.html', context)
+
+    return render(request, 'result.html', context)
